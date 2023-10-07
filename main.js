beb8454b971f50f86000901240ea6a40eb9bc1a4
Bruno Bossois Hohmann <gnobr1@gmail.com>
2023-10-06 22:05:52 -0300

    Muda o controle de audio para somente um botão

--- main.js
+++ main.js
@@ -10,6 +10,23 @@ const click_botao = document.querySelector("#botao")
 
 click_botao.addEventListener("click", AbreMenu)
 
-const musica = document.querySelector("[data-audio]")
+const musica = document.querySelector("#audio")
 
 musica.volume = 0.5
+musica.play()
+
+const TocarMusica = () => {
+    if (document.querySelector("#audio").paused == true) {
+        document.querySelector("#audio").play()
+        botao.classList.toggle("fa-pause")
+        botao.classList.toggle("fa-play")
+    } else {
+        document.querySelector("#audio").pause()
+        botao.classList.toggle("fa-pause")
+        botao.classList.toggle("fa-play")
+    }
+}
+
+const botao = document.querySelector("[data-botao-audio]")
+
+botao.addEventListener("click", TocarMusica)
